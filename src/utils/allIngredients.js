3b32f1a2536a3c070380a3edca478909c53e52a9
Gabriel-sorour <gabrielsorour51@gmail.com>
2025-12-11 16:51:07 +0200

    Add allIngredients.js mock data

---
+++ src/utils/allIngredients.js
@@ -0,0 +1,7 @@
+export const ALL_INGREDIENTS = [
+  "Tomato", "Onion", "Garlic", "Potato", "Carrot", "Cucumber", "Lettuce",
+  "Chicken Breast", "Beef", "Egg", "Salmon", "Tuna",
+  "Rice", "Pasta", "Bread", "Flour", "Sugar", "Salt", "Pepper",
+  "Milk", "Cheese", "Butter", "Yogurt", "Cream",
+  "Olive Oil", "Lemon", "Basil", "Parsley", "Chili Flakes"
+];
